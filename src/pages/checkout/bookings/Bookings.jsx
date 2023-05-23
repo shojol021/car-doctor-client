516c83ffa894e78fe1e27ff166ca75f30abb079a
shojol021 <shojol021@gmail.com>
2023-05-23 08:50:05 +0600

    booking deleone option created

--- src/pages/checkout/bookings/Bookings.jsx
+++ src/pages/checkout/bookings/Bookings.jsx
@@ -12,6 +12,23 @@ const Bookings = () => {
             .then(data => setBookings(data))
     }, [])
 
+    const handleDelete = (id) => {
+        const proceed = confirm("Are you sure want to delete?")
+        if (proceed) {
+            fetch(`http://localhost:5000/delete/${id}`, {
+                method: 'delete',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        const remaining = bookings.filter(booking => booking._id !== id)
+                        setBookings(remaining)
+                    }
+                })
+        }
+    }
+
     return (
         <div>
             <div className="overflow-x-auto">
@@ -20,17 +37,23 @@ const Bookings = () => {
                     <thead>
                         <tr>
                             <th></th>
+                            <th>Eddit/Delete</th>
                             <th>Image</th>
                             <th>Name</th>
                             <th>Email</th>
                             <th>Service</th>
                             <th>Job</th>
                             <th>Date</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         {/* row 1 */}
-                        {bookings.map((booking, idx) => <BookingsTable key={idx} idx={idx+1} booking={booking}></BookingsTable>)}
+                        {bookings.map((booking, idx) => <BookingsTable
+                            key={idx}
+                            idx={idx + 1}
+                            booking={booking}
+                            handleDelete={handleDelete}></BookingsTable>)}
                     </tbody>
                 </table>
             </div>
--- src/pages/checkout/bookings/BookingsTable.jsx
+++ src/pages/checkout/bookings/BookingsTable.jsx
@@ -1,11 +1,17 @@
 
 
-const BookingsTable = ({ booking, idx }) => {
-    const { name, email, amount, date, title, img } = booking;
+const BookingsTable = ({ booking, idx, handleDelete }) => {
+    const {_id, name, email, amount, date, title, img } = booking;
+
+    
 
     return (
         <tr>
             <th>{idx}</th>
+            <th className="space-x-4">
+                <button className="btn btn-sm btn-prmary btn-square">E</button>
+                <button onClick={() => handleDelete(_id)} className="btn btn-sm btn-error btn-square">x</button>
+            </th>
             <th>
                 <div className="avatar">
                     <div className=" w-16 rounded-xl">
@@ -18,6 +24,9 @@ const BookingsTable = ({ booking, idx }) => {
             <td>{title}</td>
             <td>{amount}</td>
             <td>{date}</td>
+            <td>
+                <button className="btn btn-sm">Details</button>
+            </td>
         </tr>
     );
 };
